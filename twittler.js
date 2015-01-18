2bfee863ab1df47005908817f5ea9d04bf253c9b
Jonathan Huang <grokut@gmail.com>
2015-01-17 16:32:01 -0800

    abstracted code to addTweet function and implemented tweetListener

--- twittler.js
+++ twittler.js
@@ -3,36 +3,49 @@
 
 $(document).ready(function(){
 
+    var index = streams.home.length - 1;
+
+    while(index >= 0){
+        var tweet = streams.home[index];
+        addTweet(tweet);
+        index -= 1;
+    }
+
+   tweetListener();
+});
+
+// Utility Functions
+
+var addTweet = function(tweet) {
+
     var $body = $('.tweets');
 
-    var index = streams.home.length - 1;
-    while(index >= 0){
+    var $tweet = $('<article class="tweet clearfix">');
+    var $tweetDetails = $('<div class="tweet-details"></div>');
+    var $tweetContent = $('<p class="tweet-content"></p>');
+    var $tweetAuthor = $('<a class="author"></a>');
+    var $tweetTime = $('<small class="time"></small>');
 
-		var tweet = streams.home[index];
-        
-	    var $tweet = $('<article class="tweet clearfix">');
-        var $tweetDetails = $('<div class="tweet-details"></div>')
-        var $tweetContent = $('<p class="tweet-content"></p>');
-        var $tweetAuthor = $('<a class="author"></a>')
-        var $tweetTime = $('<small class="time"></small>')
+    $tweetAuthor.text('@'+tweet.user);
+    $tweetTime.text(tweet.created_at);
+    $tweetContent.text(tweet.message);
 
+    $tweet.appendTo($body);
+    $tweetAuthor.appendTo($tweetDetails);
+    $tweetTime.appendTo($tweetDetails);
+    $tweetContent.appendTo($tweetDetails);
+    $tweetDetails.appendTo($tweet);
 
-	    
+};
 
-        $tweetAuthor.text('@'+tweet.user)
-        $tweetTime.text(tweet.created_at)
-        $tweetContent.text(tweet.message);
-
-        $tweet.appendTo($body);
-        $tweetAuthor.appendTo($tweetDetails)
-        $tweetTime.appendTo($tweetDetails)
-        $tweetContent.appendTo($tweetDetails)
-        $tweetDetails.appendTo($tweet)
-
-	    index -= 1
-	}
+var tweetListener = function(){
+    var lastTweeted = streams.home[streams.home.length-1];
+    addTweet(lastTweeted);
+    setTimeout(tweetListener, Math.random() * 5000);
+};
 
 
-});
+
+
 
 }());
