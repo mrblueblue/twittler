b9763d14d9ce27f4ca71c5a048e7ba6fe8b0c15f
Jonathan Huang <grokut@gmail.com>
2015-01-18 21:10:22 -0800

    module :code pattern

--- twittler.js
+++ twittler.js
@@ -1,84 +1,87 @@
 
-(function(){
 
-  // default view mode is all
+var Twittle = (function () {
+
   var viewMode = 'all';
 
-  var $stream= $('.tweets');
+  var viewToggle = {
+    shawndrost : 0,
+    sharksforcheap : 0,
+    mracus : 0,
+    douglascalhoun : 0 
+  }
 
-  var initializeTweets = function(){
+  function twittlerInitializer() {
     var index = streams.home.length - 1;
     while(index >= 0){
-      var tweet = streams.home[index];
-      addTweet(tweet);
+      var twittle = streams.home[index];
+      twittleMaker(twittle);
       index -= 1;
     }
   }
 
-  var tweetListener = function(){
-    var lastTweeted = streams.home[streams.home.length-1];
-    addTweet(lastTweeted);
-    setTimeout(tweetListener, Math.random() * 20000);
+  function twittleListener(){
+    var lastGeneratedTwittle = streams.home[streams.home.length-1];
+    twittleMaker(lastGeneratedTwittle);
+    setTimeout(twittleListener, Math.random() * 20000);
   };
 
-  var addTweet = function(tweet) {
+  function twittleMaker(tweet) {
 
-    var $tweet = $('<article class="tweet clearfix">');
-    var $tweetDetails = $('<div class="tweet-details"></div>');
-    var $tweetMessage = $('<p class="tweet-content"></p>');
-    var $username = $('<a class="author"></a>');
-    var $tweetTime = $('<small class="time"></small>');
+    var $stream = $('.tweets');  
 
-    var $avatar = {
-      shawndrost : $("<a class='avatar'><img src='img/shawndrost.jpeg'</a>"),
-      sharksforcheap : $("<a class='avatar'><img src='img/sharksforcheap.jpg'</a>"),
-      mracus : $("<a class='avatar'><img src='img/mracus.png'</a>"),
-      douglascalhoun :$("<a class='avatar'><img src='img/douglascalhoun.jpg'</a>")
-    }
+    var $twittle = $('<article class="tweet clearfix">');
+      var $details = $('<div class="tweet-details"></div>');
+        var $message = $('<p class="tweet-content"></p>');
+        var $author = $('<a class="author"></a>');
+        var $timeStamp = $('<small class="time"></small>');
 
-    if (viewMode === 'all' || viewMode === $username.text() ){
-      $tweet.prependTo($stream); 
-    } else {
-      $tweet.prependTo($stream).hide(); 
-    }
-   
-   $tweet
-      .data('author', tweet.user)
-      .append($avatar[tweet.user])
-      .append($tweetDetails);
-
-    $tweetDetails
-      .append($username.text('@'+tweet.user))
-      .append($tweetTime.text(tweet.created_at.getHours()))
-      .append($tweetMessage.text(tweet.message))
-
- 
-    $username.on('click', function(){
-
-      var authorOnClick = $(this).text()
-      var clicks = $(this).data('clicks')
-      var setClick = $(this).data("clicks", !clicks);
-      var allTweets = $('.tweet')
-      var othersTweets = $('.tweet').filter( function() {
-            return '@' + $(this).data('author') !== authorOnClick;
-          });
-
-      if (clicks) {
-        viewMode = 'all'
-        allTweets.show();
-      } else {
-        viewMode = authorOnClick
-        othersTweets.hide();
+      var $avatar = {
+        shawndrost : $("<a class='avatar'><img src='img/shawndrost.jpeg'</a>"),
+        sharksforcheap : $("<a class='avatar'><img src='img/sharksforcheap.jpg'</a>"),
+        mracus : $("<a class='avatar'><img src='img/mracus.png'</a>"),
+        douglascalhoun :$("<a class='avatar'><img src='img/douglascalhoun.jpg'</a>")
       }
 
-      setClick
+    if ( viewMode === 'all' || viewMode === $author.text() ){
+      $twittle.prependTo($stream); 
+    } else {
+      $twittle.prependTo($stream).hide(); 
+    }
+   
+   $twittle
+      .data('author', tweet.user)
+      .append($avatar[tweet.user])
+      .append($details);
+
+      $details
+        .append($author.text('@'+tweet.user))
+        .append($timeStamp.text(tweet.created_at.getHours()))
+        .append($message.text(tweet.message));
+ 
+    $author.on('click', function(){
+      var clickedAuthor = $(this).text();
+      var notAuthor = $('.tweet').filter( function() {
+            return '@' + $(this).data('author') !== clickedAuthor;
+          });
+
+      if ( $(this).data('clicks') ) {
+        viewMode = 'all'
+        $stream.show();
+      } else {
+        viewMode = clickedAuthor;
+        notAuthor.hide();
+      }
+      $(this).data("clicks", !clicks);
     });
   };
 
+  return { intialize : twittlerInitializer,
+            listen : twittleListener}
+
+})();
+
 $(document).ready(function(){
-    initializeTweets();
-    tweetListener();
+    Twittle.intialize();
+    Twittle.listen();
   });
-
-}());
-
